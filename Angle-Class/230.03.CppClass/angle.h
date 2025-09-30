e440ae3ec7a19595ac7c870eeda3a15d8501a0e7
robersan2212 <san20086@byui.edu>
2025-09-30 11:38:26 +0100

    Started angle class from compressed folder "230.03.CppClass.zip."

---
+++ Angle-Class/230.03.CppClass/angle.h
@@ -0,0 +1,44 @@
+/*************************************************************
+ * 1. Name:
+ *      -your name-
+ * 2. Assignment Name:
+ *      Practice 03: Angle Class
+ * 3. Assignment Description:
+ *      A class to represent an angle
+ * 4. What was the hardest part? Be as specific as possible.
+ *      -a paragraph or two about how the assignment went for you-
+ * 5. How long did it take for you to complete the assignment?
+ *      -total time in hours: reading the assignment, submitting, etc.
+ **************************************************************/
+
+#pragma once
+
+#define TWO_PI 6.28318530718
+
+#include <math.h>    // for floor()
+#include <iostream>  // for cout
+#include <cassert>   // for assert()
+using namespace std;
+
+class TestAngle;
+
+ /************************************
+  * ANGLE
+  ************************************/
+class Angle
+{
+   friend TestAngle;
+public:
+	// returns the angle in degrees
+	double getDegrees() const { return convertToDegrees(radians); }
+
+	//returns the angle in radians
+	double getRdians() const { return radians; }
+
+	//Takes a degrees as a parameter and updates the attribute with the passed parameter. 
+	void setDegrees(double degrees) { radians = convertToDegrees(degrees); }
+
+
+};
+
+
