9801ecc2d4de0534b2ec57d2e1ecbef2b264d58d
EmeraldE11 <77083262+EmeraldE11@users.noreply.github.com>
2025-10-04 12:35:12 -0700

    Worked through ticket three of the W3 Lab, added all project files

---
+++ 230.03.Ponder/acceleration.cpp
@@ -0,0 +1,81 @@
+/***********************************************************************
+ * Source File:
+ *    ACCELERATION 
+ * Author:
+ *    Br. Helfrich
+ * Summary:
+ *    Everything we need to know about changing speed
+ ************************************************************************/
+
+#include "acceleration.h"
+#include "angle.h"
+
+#include <cmath>
+
+ /*********************************************
+  * ACCELERATION : Constructors
+  *  Default - sets ddx and ddy to 0
+  *  Parameterized - sets ddx and ddy to the passed parameters
+  *********************************************/
+
+Acceleration::Acceleration()
+{
+	ddx = 0.0;
+	ddy = 0.0;
+}
+
+Acceleration::Acceleration(double x, double y)
+{
+	ddx = x;
+	ddy = y;
+}
+
+
+/*********************************************
+ * ACCELERATION : ADD
+ *  a += a
+ *********************************************/
+void Acceleration::add(const Acceleration& acceleration)
+{
+   ddx = -99.9;
+   ddy = -88.8;
+}
+
+/*********************************************
+ * ACCELERATION : SET
+ *  set from angle and direction
+ *********************************************/
+void Acceleration::set(const Angle & angle, double magnitude)
+{
+	double radians = angle.getDegrees() * M_PI / 180.0; // cos and sin need doubles, this uses the angle to pull the radians we need.
+	ddx = magnitude * std::cos(radians);
+	ddy = magnitude * std::sin(radians);
+}
+
+/*********************************************
+ * ACCELERATION : Getters
+ *  pulls the value for ddx and ddy
+ *********************************************/
+double Acceleration::getDDX() const
+{
+	return ddx;
+}
+
+double Acceleration::getDDY() const
+{
+	return ddy;
+}
+
+/*********************************************
+ * ACCELERATION : Setters
+ *  updates the values of ddx and ddy
+ *********************************************/
+void Acceleration::setDDX(double x)
+{
+	ddx = x;
+}
+
+void Acceleration::setDDY(double y)
+{
+	ddy = y;
+}
