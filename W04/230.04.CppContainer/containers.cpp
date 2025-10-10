84447ecfc3e7bf5565d34355469a849f888bb333
Spencer Palmer <monkies989@gmail.com>
2025-10-09 21:56:58 -0600

    Reuploaded the assignment and applied my changes

---
+++ W04/230.04.CppContainer/containers.cpp
@@ -0,0 +1,79 @@
+/*************************************************************
+ * 1. Name:
+ *      Roberto Sanchez, Spencer Palmer, Noah McCarthy
+ * 2. Assignment Name:
+ *      Practice 04: Contaiers
+ * 3. Assignment Description:
+ *      Work with a list and a vector
+ * 4. What was the hardest part? Be as specific as possible.
+ *      -a paragraph or two about how the assignment went for you-
+ * 5. How long did it take for you to complete the assignment?
+ *      -total time in hours: reading the assignment, submitting, etc.
+ **************************************************************/
+
+
+#include "testContainers.h"
+#include <string>
+#include <list>
+#include <vector>
+using namespace std;
+
+list<string> arrayToList(string a[], int num)
+{
+   list<string> result;
+   for (int i = 0; i < num; i++)
+   {
+      result.push_back(a[i]);
+   }
+   return result;
+}
+
+/*****************************
+ * ARRAY TO LIST BACKWARDS
+ *****************************/
+list<string> arrayToListBackwards(string a[], int num)
+{
+   list<string> result;
+   for (int i = 0; i < num; i++)
+   {
+      result.push_front(a[i]);
+   }
+   return result;
+}
+
+/*****************************
+ * LIST TO VECTOR
+ *****************************/
+vector<string> listToVector(const list<string> & l)
+{
+   vector<string> result(l.begin(), l.end());
+    
+   return result;
+}
+
+/*****************************
+ * LIST TO VECTOR BACKWARDS
+ *****************************/
+vector<string> listToVectorBackwards(const list<string>& l)
+{
+   vector<string> result(l.rbegin(), l.rend());
+
+   return result;
+}
+
+/*****************************
+ * FIND ELEMENT IN VECTOR
+ *****************************/
+bool findElementInVector(const vector<string> & v, const string& element)
+{
+   return false;
+}
+
+/*****************************
+ * MAIN
+ *****************************/
+int main()
+{
+   TestCollection().run();
+   return 0;
+}
