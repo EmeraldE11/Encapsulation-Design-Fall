63cec26cc6b1db86e70e9b19ab26d1cac196eb2a
Spencer Palmer <monkies989@gmail.com>
2025-11-04 18:12:57 -0700

    Created program and wrote necessary code for assignment.

---
+++ 230.08.CppAllocation/allocate.h
@@ -0,0 +1,58 @@
+/*************************************************************
+ * 1. Name:
+ *      Spencer Palmer, Noah McCarthy, Roberto Sanchez
+ * 2. Assignment Name:
+ *      Practice 08: Allocation
+ * 3. Assignment Description:
+ *      Allocate an array and then use it
+ * 4. What was the hardest part? Be as specific as possible.
+ *      The hardest part was figuring out how to write the delete functions and get the results we wanted in the tests
+ * 5. How long did it take for you to complete the assignment?
+ *      -total time in hours: 2 hrs
+ **************************************************************/
+
+/****************************
+* ALLOCATE ONE FLOAT
+****************************/
+inline float* allocateOneFloat(float f)
+{
+    float* p = new float;
+    *p = f;
+    return p;
+}
+
+
+/****************************
+ * ALLOCATE ARRAY of DOUBLEs
+ ****************************/
+inline double* allocateArrayDouble(int count)
+{
+    if (count <= 0)
+        return nullptr;
+    
+    return new double[count];
+}
+
+/****************************
+ * DELETE ONE FLOAT
+ ****************************/
+inline void deleteOneFloat(float*& p)
+{
+    if (p != nullptr)
+    {
+        delete p;
+        p = nullptr;
+    }
+}
+
+/****************************
+ * DELETE ARRAY of DOUBLEs
+ ****************************/
+inline void deleteArrayDouble(double*& p)
+{
+    if (p != nullptr)
+    {
+        delete[] p;
+        p = nullptr;
+    }
+}
